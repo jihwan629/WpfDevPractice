89faa720a29bae0b4248a05f20476c2d8bf62694
jihwan629 <jihwan629@naver.com>
2021-01-15 18:47:29 +0900

    Example3

--- BikeShop/BikeShop/Menu.xaml.cs
+++ BikeShop/BikeShop/Menu.xaml.cs
@@ -31,5 +31,12 @@ namespace BikeShop
                 new Uri("/Contact.xaml", UriKind.Relative)
                 );
         }
+
+        private void LiveSupportBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(
+                new Uri("/Discussion.xaml", UriKind.Relative)
+                );
+        }
     }
 }
