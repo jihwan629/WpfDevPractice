b601b3a32e02dfad8ff1358170303418892a8660
jihwan629 <jihwan629@naver.com>
2021-01-29 02:35:55 +0900

    CurrencyConverter

---
+++ WpfBasicControlApp4/WpfBasicControlApp4/CurrencyConverterViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBasicControlApp4
+{
+    public class CurrencyConverterViewModel : Notifier
+    {
+        private decimal euros;
+        
+        public decimal Euros
+        {
+            get { return euros; }
+            set
+            {
+                euros = value;
+                OnPropertyChanged("Euros");
+                OnEurosChanged();
+            }
+        }
+
+        private decimal dolloars;
+
+        public decimal Dollars
+        {
+            get { return dolloars; }
+            set
+            {
+                dolloars = value;
+                OnPropertyChanged("Dollars");
+            }
+        }
+
+        private void OnEurosChanged()
+        {
+            Dollars = Euros * 1.1M;
+        }
+    }
+}
